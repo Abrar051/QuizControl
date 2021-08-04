c3c4bacfcf7d5a7bb4e2409ecba88c3b1a5d92c5
Abrar051 <75354845+Abrar051@users.noreply.github.com>
2021-08-04 19:39:16 +0600

    Update QuizControlFinal.sql

--- QuizControlFinal.sql
+++ QuizControlFinal.sql
@@ -1,21 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 04, 2021 at 03:36 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.3.28
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
 
 --
 -- Database: `quizcontrol`
@@ -110,19 +100,16 @@ ALTER TABLE `userinfo`
 ALTER TABLE `author`
   ADD CONSTRAINT `author_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `examinfo` (`Id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
---
+
 -- Constraints for table `examinfo`
---
+
 ALTER TABLE `examinfo`
   ADD CONSTRAINT `examinfo_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `userinfo` (`Id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
---
+
 -- Constraints for table `examprofile`
---
+
 ALTER TABLE `examprofile`
   ADD CONSTRAINT `examprofile_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `userinfo` (`Id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
